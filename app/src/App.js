9e5176ce3b5447084b5157539c609370fc6cdf6f
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-03-04 10:10:46 -0600

    initial

---
+++ app/src/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    state = { 
+        isLoading: false,
+        invoices: []
+    }
+
+    //
+    render() {
+        const isLoading = this.state.isLoading;
+        const 
+        const body =
+            [
+                {
+                    "id": "100",
+                    "Vendor": "Hankook",
+                    "Amount": "$1800",
+                    "Date": "08/21/2019"
+                },
+                {
+                    "id": "100",
+                    "Vendor": "Hankook",
+                    "Amount": "$1800",
+                    "Date": "08/21/2019"
+                },
+                {
+                    "id": "100",
+                    "Vendor": "Hankook",
+                    "Amount": "$1800",
+                    "Date": "08/21/2019"
+                },
+                {
+                    "id": "100",
+                    "Vendor": "Hankook",
+                    "Amount": "$1800",
+                    "Date": "08/21/2019"
+                },
+            ]
+        return (
+            
+        );
+    }
+}
+
+export default App;
