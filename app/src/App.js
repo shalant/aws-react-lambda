3d2b6b0449b9b7883faed6cc3cadde9e38235739
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-03-05 16:01:23 -0600

    51:08, starting on s3 bucket

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import {Table, Button} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faImage, faThumbsDown, faMoneyCheckAlt, faSearchDollar, faThumbsUp} from '@fortawesome/free-solid-svg-icons';
+
 
 class App extends Component {
 
@@ -37,8 +40,9 @@ class App extends Component {
     }
 
     remove(id){
+        console.log(id);
         let updatedInvoices = [...this.state.invoices].filter(i => i.id !== id)
-        this.setState({invoice : updatedInvoices})
+        this.setState({invoices : updatedInvoices})
     }
 
     render() {
@@ -56,31 +60,36 @@ class App extends Component {
                     <td>{invoice.Amount}</td>
                     <td>{invoice.Invoice}</td>
                     <td>{invoice.Date}</td>
-                    <td><Button className='btn btn-lg btn-success' onClick={() => this.remove(invoice.id)}>OK</Button></td>
-                    <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(invoice.id)}>NOK</Button></td>
-                    <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(invoice.id)}>50%</Button></td>
-                    <td><Button className='btn btn-lg btn-warning' onClick={() => this.remove(invoice.id)}>??</Button></td>
-                    <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(invoice.id)}>Image</Button></td>
+                    <td><Button className='btn btn-lg btn-success' onClick={() => this.remove(invoice.id)}> <FontAwesomeIcon icon={faThumbsUp} /> OK</Button></td>
+                    <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(invoice.id)}> <FontAwesomeIcon icon={faThumbsDown} /> NOK</Button></td>
+                    <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(invoice.id)}><FontAwesomeIcon icon={faMoneyCheckAlt} />50%</Button></td>
+                    <td><Button className='btn btn-lg btn-warning' onClick={() => this.remove(invoice.id)}><FontAwesomeIcon icon={faSearchDollar} />??</Button></td>
+                    <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(invoice.id)}><FontAwesomeIcon icon={faImage} />Image</Button></td>
                 </tr>
                 )
 
         return (
+
             <div className='container border border-secondary rounded center'>
+
                 <div className='row'>
                     <div className='col-12'>
                         <h4>Pending Invoices - The Test Company</h4>
                     </div>
                 </div>
-                <div className='row'>
+                <div className="row">
                     <div className=".col-xs-12 center text-center">
                         <Table dark responsive striped bordered hover>
                             <thead>
-                                <th>Vendor</th>
-                                <th>Amount</th>
-                                <th>Invoice #</th>
-                                <th>Date</th>
-                                <th colSpan="4">Actions</th>
-                                <th>Image</th>
+                                <tr>
+                                    <th>Vendor</th>
+                                    <th>Amount</th>
+                                    <th>Invoice #</th>
+                                    <th>Date</th>
+                                    <th colSpan="4">Actions</th>
+                                    <th>Image</th>
+                                </tr>
+                                
                             </thead>
                             <tbody>
                                 {this.state.invoices.length === 0 ? <td colSpan="9">All Caught Up!</td> : invoices}
