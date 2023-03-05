d2e4a3860e6955e4718925e6f37576e3c115b63a
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-03-04 21:54:45 -0600

    38:36 complex react state

--- app/src/App.js
+++ app/src/App.js
@@ -1,44 +1,97 @@
 import React, {Component} from 'react';
+import {Table, Button} from 'reactstrap';
 
 class App extends Component {
+
     state = { 
         isLoading: false,
-        invoices: []
+        invoices: [
+            {
+            "id": "100",
+            "Vendor": "Hankook",
+            "Amount": "$1800",
+            "Invoice": "1123",
+            "Date": "08/21/2019"
+        },
+        {
+            "id": "200",
+            "Vendor": "Hankook",
+            "Amount": "$1800",
+            "Invoice": "1123",
+            "Date": "08/21/2019"
+        },
+        {
+            "id": "300",
+            "Vendor": "Hankook",
+            "Amount": "$1800",
+            "Invoice": "1123",
+            "Date": "08/21/2019"
+        },
+        {
+            "id": "400",
+            "Vendor": "Hankook",
+            "Amount": "$1800",
+            "Invoice": "1123",
+            "Date": "08/21/2019"
+        },]
     }
 
-    //
+    remove(id){
+        let updatedInvoices = [...this.state.invoices].filter(i => i.id !== id)
+        this.setState({invoice : updatedInvoices})
+    }
+
     render() {
         const isLoading = this.state.isLoading;
-        const 
-        const body =
-            [
-                {
-                    "id": "100",
-                    "Vendor": "Hankook",
-                    "Amount": "$1800",
-                    "Date": "08/21/2019"
-                },
-                {
-                    "id": "100",
-                    "Vendor": "Hankook",
-                    "Amount": "$1800",
-                    "Date": "08/21/2019"
-                },
-                {
-                    "id": "100",
-                    "Vendor": "Hankook",
-                    "Amount": "$1800",
-                    "Date": "08/21/2019"
-                },
-                {
-                    "id": "100",
-                    "Vendor": "Hankook",
-                    "Amount": "$1800",
-                    "Date": "08/21/2019"
-                },
-            ]
+        const allinvoices = this.state.invoices;
+
+            if(isLoading)
+                return(<div>Loading...</div>);
+
+
+            let invoices = 
+            allinvoices.map(invoice =>
+                <tr key={invoice.id}>
+                    <td>{invoice.Vendor}</td>
+                    <td>{invoice.Amount}</td>
+                    <td>{invoice.Invoice}</td>
+                    <td>{invoice.Date}</td>
+                    <td><Button className='btn btn-lg btn-success' onClick={() => this.remove(invoice.id)}>OK</Button></td>
+                    <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(invoice.id)}>NOK</Button></td>
+                    <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(invoice.id)}>50%</Button></td>
+                    <td><Button className='btn btn-lg btn-warning' onClick={() => this.remove(invoice.id)}>??</Button></td>
+                    <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(invoice.id)}>Image</Button></td>
+                </tr>
+                )
+
         return (
-            
+            <div className='container border border-secondary rounded center'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <h4>Pending Invoices - The Test Company</h4>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className=".col-xs-12 center text-center">
+                        <Table dark responsive striped bordered hover>
+                            <thead>
+                                <th>Vendor</th>
+                                <th>Amount</th>
+                                <th>Invoice #</th>
+                                <th>Date</th>
+                                <th colSpan="4">Actions</th>
+                                <th>Image</th>
+                            </thead>
+                            <tbody>
+                                {this.state.invoices.length === 0 ? <td colSpan="9">All Caught Up!</td> : invoices}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+
+
+
+            </div>
         );
     }
 }
